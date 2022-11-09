afff5aa9717138e823c4e2022d16f31bf9bd8d64
Matthew Leng <60058291+Mleng89@users.noreply.github.com>
2022-11-08 21:36:45 -0500

    Controller-Jokes: Created a Jokes controller

---
+++ Controllers/JokesController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using JokeWebApp.Data;
+using JokeWebApp.Models;
+
+namespace JokeWebApp.Controllers
+{
+    // [Route("[controller]")]
+    public class JokesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public JokesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
