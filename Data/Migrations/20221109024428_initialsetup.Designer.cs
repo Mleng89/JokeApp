6afd718e93f84f73469e6cdde25944bf9bff7849
Matthew Leng <60058291+Mleng89@users.noreply.github.com>
2022-11-08 21:50:48 -0500

    DATA-MIGRATIONS: Initialsetup-Joke Model

--- Data/Migrations/20221109024428_initialsetup.Designer.cs
+++ Data/Migrations/20221109024428_initialsetup.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using JokeWebApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace JokeWebApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221109024428_initialsetup")]
+    partial class initialsetup
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.10");
