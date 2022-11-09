d614412cce736997cf76a8f03c6d4b34cc7a7c24
Matthew Leng <60058291+Mleng89@users.noreply.github.com>
2022-11-08 20:14:56 -0500

    Models: Jokes added

---
+++ Models/Jokes.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokeWebApp.Models
+{
+    public class Jokes
+    {
+        public int Id { get; set; }
+        public string? JokeQuestion { get; set; }
+        public string? JokeAnswer { get; set; }
+    }
+}
